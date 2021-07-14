8c94e239bdd56e7e8ed0638ff2a6b6ddf4f6bcca
Peter Phan <pphan@hashicorp.com>
2021-07-14 10:07:55 -0700

    Update sub-2a

--- test-workspace-triggers/sub-1/sub-2a/main.tf
+++ test-workspace-triggers/sub-1/sub-2a/main.tf
@@ -1,24 +1,3 @@
-resource "random_pet" "server" {
-  length = var.pet_name_length
-  prefix = var.prefix
-}
-
-# variables.tf
-
-variable "pet_name_length" {
-  type    = string
-  default = "1"
-}
-
-variable "prefix" {
-  default = "test"
-}
-
-# output.tf
-output "server" {
-  value = random_pet.server.id
-}
-
 output "workspace" {
-  value = "sub-2a"
+  value = "sub-2a "
 }
