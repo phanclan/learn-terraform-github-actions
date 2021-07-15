0ecf4e008491d46bebc332deb36d00e0bc3cd79e
Peter Phan <pphan@hashicorp.com>
2021-07-14 17:51:21 -0700

    updates

--- test-workspace-triggers/sub-1/main.tf
+++ test-workspace-triggers/sub-1/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-  backend "remote" {
-    organization = "pphan"
-
-    workspaces {
-      name = "sub-1"
-    }
-  }
-}
 resource "random_pet" "server" {
   length = var.pet_name_length
   prefix = var.prefix
